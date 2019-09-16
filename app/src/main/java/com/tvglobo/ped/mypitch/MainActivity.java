0f9f37fd08aca924d8b28a263219b4a141a0adc8
Thiago Frensch <thiago.frensch@tvglobo.com.br>
2019-09-16 17:54:35 -0300

    removendo teste com acelerometro

--- app/src/main/java/com/tvglobo/ped/mypitch/MainActivity.java
+++ app/src/main/java/com/tvglobo/ped/mypitch/MainActivity.java
@@ -4,10 +4,6 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -40,7 +36,7 @@ import com.softmoore.android.graphlib.GraphView;
 import com.softmoore.android.graphlib.Label;
 import com.softmoore.android.graphlib.Point;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+public class MainActivity extends AppCompatActivity {
 
     AudioDispatcher dispatcher = null;
 
@@ -50,29 +46,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView noteText;
 
     private static final String TAG = "MainActivity";
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-    private  Sensor sensors;
-
     private LineChart mChart;
     private Thread thread;
     private boolean plotData = true;
 
     protected void onCreateChart() {
         setContentView(R.layout.activity_main);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-
-        List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-
-        for(int i=0; i<sensors.size(); i++){
-            Log.d(TAG, "onCreate: Sensor "+ i + ": " + sensors.get(i).toString());
-        }
-
-        if (mAccelerometer != null) {
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
-        }
 
         mChart = (LineChart) findViewById(R.id.chart1);
 
@@ -161,36 +140,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
-    private void addEntry(SensorEvent event) {
-
-        LineData data = mChart.getData();
-
-        if (data != null) {
-
-            ILineDataSet set = data.getDataSetByIndex(0);
-            // set.addEntry(...); // can be called as well
-
-            if (set == null) {
-                set = createSet();
-                data.addDataSet(set);
-            }
-
-//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 80) + 10f), 0);
-            data.addEntry(new Entry(set.getEntryCount(), event.values[0] + 5), 0);
-            data.notifyDataChanged();
-
-            // let the chart know it's data has changed
-            mChart.notifyDataSetChanged();
-
-            // limit the number of visible entries
-            mChart.setVisibleXRangeMaximum(150);
-            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
-
-            // move to the latest entry
-            mChart.moveViewToX(data.getEntryCount());
-
-        }
-    }
 
     private LineDataSet createSet() {
 
@@ -238,21 +187,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (thread != null) {
             thread.interrupt();
         }
-        mSensorManager.unregisterListener(this);
-
-    }
-
-    @Override
-    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Do something here if sensor accuracy changes.
-    }
-
-    @Override
-    public final void onSensorChanged(SensorEvent event) {
-        /*if(plotData){
-            addEntry(event);
-            plotData = false;
-        }*/
     }
 
     public final void onNoteChanged(float value) {
@@ -264,14 +198,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
-    }
-
-    @Override
     protected void onDestroy() {
-        mSensorManager.unregisterListener(MainActivity.this);
         thread.interrupt();
         super.onDestroy();
     }
