563465c6bebb118d9786420f59713e213db7a764
Thiago Frensch <thiago.frensch@tvglobo.com.br>
2019-09-30 10:40:44 -0300

    fundo preto e layout de botões e texto

--- app/src/main/java/com/tvglobo/ped/mypitch/TuneTest.java
+++ app/src/main/java/com/tvglobo/ped/mypitch/TuneTest.java
@@ -19,7 +19,6 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
@@ -32,8 +31,8 @@ import be.tarsos.dsp.pitch.PitchProcessor;
 public class TuneTest extends AppCompatActivity {
     AudioDispatcher dispatcher = null;
 
-    String[] notes = {"C2",   "C#2",  "D2",   "D#2",  "E2",   "F2",   "F#2",  "G2",  "G#2",    "A2",    "A#2",   "B2",    "C3",    "C#3",   "D3",    "D#3",   "E3",    "F3",    "F#3",   "G3",    "G#3",   "A3",    "A#3",   "B3",    "C4",    "C#4",   "D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5"};
-    Float[] freqs = { 65.41f, 69.30f, 73.42f, 77.78f, 82.41f, 87.31f, 92.50f, 98.00f, 103.83f, 110.00f, 116.54f, 123.47f, 130.81f, 138.59f, 146.83f, 155.56f, 164.81f, 174.61f, 185.00f, 196.00f, 207.65f, 220.00f, 233.08f, 246.94f, 261.63f, 277.18f, 293.66f, 311.13f, 329.63f, 349.23f, 369.99f, 392.00f, 415.30f, 440.00f, 466.16f, 493.88f, 523.25f, 554.37f, 587.33f, 622.25f, 659.25f, 698.46f, 739.99f, 783.99f, 830.61f, 880.00f, 932.33f, 987.77f};
+    String[] notes = {"B1",   "C2",   "C#2",  "D2",   "D#2",  "E2",   "F2",   "F#2",  "G2",  "G#2",    "A2",    "A#2",   "B2",    "C3",    "C#3",   "D3",    "D#3",   "E3",    "F3",    "F#3",   "G3",    "G#3",   "A3",    "A#3",   "B3",    "C4",    "C#4",   "D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5","C6"};
+    Float[] freqs = { 61.73f, 65.41f, 69.30f, 73.42f, 77.78f, 82.41f, 87.31f, 92.50f, 98.00f, 103.83f, 110.00f, 116.54f, 123.47f, 130.81f, 138.59f, 146.83f, 155.56f, 164.81f, 174.61f, 185.00f, 196.00f, 207.65f, 220.00f, 233.08f, 246.94f, 261.63f, 277.18f, 293.66f, 311.13f, 329.63f, 349.23f, 369.99f, 392.00f, 415.30f, 440.00f, 466.16f, 493.88f, 523.25f, 554.37f, 587.33f, 622.25f, 659.25f, 698.46f, 739.99f, 783.99f, 830.61f, 880.00f, 932.33f, 987.77f, 1046.50f};
     Button buttonRec;
     Button buttonTone;
 
@@ -51,7 +50,7 @@ public class TuneTest extends AppCompatActivity {
     private float noteMaxFreq;
     private float noteMinFreq;
     private boolean recValues = false;
-    private ArrayList<Float> valuesSaved = new ArrayList<Float>();
+    private ArrayList<Float> valuesSaved = new ArrayList();
     private PlayTone playTone = null;
 
     @Override
@@ -86,7 +85,6 @@ public class TuneTest extends AppCompatActivity {
             }
         });
 
-        //noteIndex = Arrays.binarySearch(notes, noteToTest);
         noteIndex = -1;
         for (int i = 0; i < notes.length; ++i) {
             if (noteToTest.equals(notes[i])) {
@@ -109,15 +107,15 @@ public class TuneTest extends AppCompatActivity {
     protected void addLimitLine(float freq, String name, int color) {
         LimitLine ll = new LimitLine(freq, name);
         ll.setLineWidth(1f);
-        //ll.enableDashedLine(10f, 10f, 0f);
         ll.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
         ll.setTextSize(10f);
+        ll.setTextColor(Color.WHITE);
         ll.setLineColor(getApplicationContext().getResources().getColor(color));
         mChart.getAxisLeft().addLimitLine(ll);
     }
     protected void onCreateChart() {
 
-        mChart = (LineChart) findViewById(R.id.chart1);
+        mChart = findViewById(R.id.chart1);
 
         // enable description text
         mChart.getDescription().setEnabled(true);
@@ -134,10 +132,10 @@ public class TuneTest extends AppCompatActivity {
         mChart.setPinchZoom(true);
 
         // set an alternative background color
-        mChart.setBackgroundColor(Color.WHITE);
+        mChart.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorGraphBG));
 
         LineData data = new LineData();
-        data.setValueTextColor(Color.WHITE);
+        data.setValueTextColor(getApplicationContext().getResources().getColor(R.color.colorGraphBG));
 
         // add empty data
         mChart.setData(data);
@@ -147,16 +145,16 @@ public class TuneTest extends AppCompatActivity {
 
         // modify the legend ...
         l.setForm(Legend.LegendForm.LINE);
-        l.setTextColor(Color.WHITE);
+        l.setTextColor(getApplicationContext().getResources().getColor(R.color.colorGraphBG));
 
         XAxis xl = mChart.getXAxis();
-        xl.setTextColor(Color.WHITE);
+        xl.setTextColor(getApplicationContext().getResources().getColor(R.color.colorGraphBG));
         xl.setDrawGridLines(true);
         xl.setAvoidFirstLastClipping(true);
         xl.setEnabled(true);
 
         YAxis leftAxis = mChart.getAxisLeft();
-        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setTextColor(getApplicationContext().getResources().getColor(R.color.colorGraphBG));
         leftAxis.setDrawGridLines(false);
         leftAxis.setAxisMaximum(noteMaxFreq);
         leftAxis.setAxisMinimum(noteMinFreq);
@@ -172,9 +170,7 @@ public class TuneTest extends AppCompatActivity {
 
         leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
         addLimitLine(freqs[noteIndex-1], notes[noteIndex-1],R.color.colorBad );
-        //addLimitLine((noteFreq+freqs[noteIndex-1])/2, "limit",R.color.colorBad );
         addLimitLine(noteFreq, noteToTest, R.color.colorGood);
-        //addLimitLine((noteFreq+freqs[noteIndex+1])/2, "limit", R.color.colorBad);
         addLimitLine(freqs[noteIndex+1], notes[noteIndex+1], R.color.colorBad);
 
         feedMultiple();
@@ -221,7 +217,7 @@ public class TuneTest extends AppCompatActivity {
         LineDataSet set = new LineDataSet(null, "Dynamic Data");
         set.setAxisDependency(YAxis.AxisDependency.LEFT);
         set.setLineWidth(3f);
-        set.setColor(Color.MAGENTA);
+        set.setColor(getApplicationContext().getResources().getColor(R.color.colorLine));
         set.setHighlightEnabled(false);
         set.setDrawValues(false);
         set.setDrawCircles(false);
@@ -264,20 +260,9 @@ public class TuneTest extends AppCompatActivity {
         }
     }
 
-    public final void onNoteChanged(float value) {
-        if(plotData){
-            addValue(value);
-            plotData = false;
-            if (recValues)
-                addRecValue(value);
-
-        }
-    }
-
     private void addRecValue(float value) {
         valuesSaved.add(value);
     }
-
 
     @Override
     protected void onDestroy() {
@@ -334,6 +319,8 @@ public class TuneTest extends AppCompatActivity {
         if (!note.isEmpty()) {
             noteText.setText(note);
             addValue(mFreq);
+            if (recValues)
+                addRecValue(pitchInHz);
         }
     }
 }
